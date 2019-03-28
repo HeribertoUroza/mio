061c3828b83529dd46d1bf35dc7f666f22c4e9b2
Heriberto Uroza <heribertouroza@pursuit.org>
2019-03-27 21:48:28 -0400

    Updated file structure to assist with node modules

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { HashRouter, Route, Switch } from 'react-router-dom'
+import Login from './containers/Login'
+
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <HashRouter>
+          <> 
+            <Switch>
+              <Route path='/Login' exact component= { Login } />
+            </Switch>
+          </>
+
+        </HashRouter>
+      </>
+
+    );
+  }
+}
+
+export default App;
---
+++ src/containers/Login.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class Login extends Component {
+    render() {
+        return (
+            <>
+                <h1>Hello World!</h1>
+            </>
+
+        );
+    }
+}
+
+export default Login;
